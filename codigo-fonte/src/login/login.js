89d6b2aaf1ffe697be73afa2de848379a19ea304
Hiago <lutahiago@gmail.com>
2024-04-28 17:37:27 -0300

    tela de login

--- codigo-fonte/src/login/login.js
+++ codigo-fonte/src/login/login.js
@@ -1,3 +1,16 @@
-function click() {
-    alert("oi")
+function mostrarAlerta(event) {
+    event.preventDefault();
+
+    var login = document.getElementById("Login").value;
+    var senha = document.getElementById("senha").value;
+
+    alert("Login: " + login + "; Senha: " + senha);
+}
+
+function clickGoogle() {
+    alert("Conecte o Login")
+}
+
+function clickFacebook() {
+    alert("Conecte o facebook")
 }
