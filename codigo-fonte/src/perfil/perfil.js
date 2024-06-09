da1654112f7e4b1fbf99160ef80682a1a9e2df91
WinnieYvelise <wyvelisebrandao@gmail.com>
2024-06-09 15:14:56 -0300

    finalização de tela

--- codigo-fonte/src/perfil/perfil.js
+++ codigo-fonte/src/perfil/perfil.js
@@ -1,25 +1,57 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.querySelector('.formulario');
+document.addEventListener('DOMContentLoaded', function () {
+    const usuarios = JSON.parse(localStorage.getItem('listUser'));
+    let usuarioLogado = localStorage.getItem('usuarioLogado');
+
+    if (!usuarios) {
+        alert("Não tem usuários cadastrados!");
+        window.location.href = '../cadastro/index.html';
+    }
+
+    const indexUsuario = buscaUsuarioDaListaDeUsuarios();
+
+    /* 
+     * Busca usuários da lista de usuários (listUser) do localStorage.
+     * Será comparado o e-mail do usuário logado que está no localStorage quando é feito o login.
+     * 
+     */
+    function buscaUsuarioDaListaDeUsuarios() {
+        for (let i = 0; i < usuarios.length; i++) {
+            const usuario = usuarios[i];
+
+            if (usuarioLogado == usuario.emailCad) {
+                return i;
+            } else {
+                return -1;
+            }
+        }
+    }
+
+    if (!usuarioLogado || indexUsuario < 0) {
+        alert("Usuário não está logado, favor realizar novo login!");
+        window.location.href = '../login/login.html';
+    }
+
+    const form = document.getElementById('formulario-perfil');
     const cancelButton = document.getElementById('cancelar-btn');
     const confirmButton = document.getElementById('confirmar-btn');
     const changePhotoButton = document.getElementById('change-photo-btn');
     const fileInput = document.getElementById('file-input');
     const avatar = document.getElementById('avatar');
-    const nomeDisplay = document.getElementById('nome-display');
-    const nomeInput = document.getElementById('nome');
+    const nomeUsuario = document.getElementById('nomeUsuario');
+
 
     const fields = {
-        nome: nomeInput,
-        email: document.getElementById('email'),
-        telefone: document.getElementById('telefone'),
-        profissao: document.getElementById('profissao'),
-        localizacao: document.getElementById('localizacao'),
-        biografia: document.getElementById('biografia')
+        campoNome: document.getElementById('campoNome'),
+        campoEmail: document.getElementById('campoEmail'),
+        campoTelefone: document.getElementById('campoTelefone'),
+        campoProfissao: document.getElementById('campoProfissao'),
+        campoCidade: document.getElementById('campoCidade'),
+        campoBiografia: document.getElementById('campoBiografia')
     };
 
     // Função para carregar a imagem do avatar do localStorage
     function loadAvatar() {
-        const storedAvatar = localStorage.getItem('avatar');
+        const storedAvatar = usuarios[indexUsuario].avatar
         if (storedAvatar) {
             avatar.src = storedAvatar;
         }
@@ -27,64 +59,26 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Função para salvar a imagem no localStorage
     function saveAvatar(imageData) {
-        localStorage.setItem('avatar', imageData);
+        usuarios[indexUsuario].avatar = imageData;
+        localStorage.setItem('listUser', JSON.stringify(usuarios));
     }
 
     // Função para carregar os dados do formulário do localStorage
     function loadFormData() {
-        Object.keys(fields).forEach(key => {
-            const value = localStorage.getItem(key);
-            if (value) {
-                fields[key].value = value;
-                if (key === 'nome') {
-                    nomeDisplay.textContent = value;
-                    nomeInput.value = value;
-                    nomeInput.classList.add('hidden');
-                }
-            }
-        });
-    }
-
-    // Função para salvar os dados do formulário no localStorage
-    function saveFormData() {
-        Object.keys(fields).forEach(key => {
-            localStorage.setItem(key, fields[key].value);
-        });
-    }
-
-    // Função para validar o telefone
-    function validateTelefone() {
-        let telefone = fields.telefone.value.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
-        if (telefone.length > 2) {
-            telefone = `(${telefone.substring(0, 2)}) ${telefone.substring(2)}`;
+        if (!usuarios[indexUsuario]) {
+            return;
         }
-        fields.telefone.value = telefone;
 
-        if (telefone.replace(/\D/g, '').length < 11) {
-            fields.telefone.style.color = 'red';
-        } else {
-            fields.telefone.style.color = '';
-        }
-    }
+        // Inicializa os campos do formulário com o valor do usuário logado encontrado no localStorage
+        fields.campoNome.value = usuarios[indexUsuario].namePersonCad;
+        fields.campoEmail.value = usuarios[indexUsuario].emailCad;
+        fields.campoTelefone.value = usuarios[indexUsuario].cellphoneNumberCad;
+        fields.campoProfissao.value = usuarios[indexUsuario].profissao;
+        fields.campoCidade.value = usuarios[indexUsuario].cidade;
+        fields.campoBiografia.value = usuarios[indexUsuario].biografia;
 
-    // Função para validar o email
-    function validateEmail() {
-        const email = fields.email.value;
-        if (!email.includes('@')) {
-            fields.email.style.color = 'red';
-        } else {
-            fields.email.style.color = '';
-        }
-    }
-
-    // Função para validar a biografia
-    function validateBiografia() {
-        const biografia = fields.biografia.value;
-        if (biografia.length < 20) {
-            fields.biografia.style.color = 'red';
-        } else {
-            fields.biografia.style.color = '';
-        }
+        // Inicializa o h2 que mostra o nome do usuário
+        nomeUsuario.innerHTML = usuarios[indexUsuario].namePersonCad;
     }
 
     // Carregar dados e avatar ao iniciar a página
@@ -92,24 +86,24 @@ document.addEventListener('DOMContentLoaded', function() {
     loadFormData();
 
     // Mostrar o campo de entrada do nome ao clicar no <h2>
-    nomeDisplay.addEventListener('click', function() {
-        nomeDisplay.classList.add('hidden');
-        nomeInput.classList.remove('hidden');
-        nomeInput.focus();
+    nomeUsuario.addEventListener('click', function () {
+        nomeUsuario.classList.add('hidden');
+        nomeUsuario.classList.remove('hidden');
+        nomeUsuario.focus();
     });
 
     // Evento do botão alterar foto
-    changePhotoButton.addEventListener('click', function(event) {
+    changePhotoButton.addEventListener('click', function (event) {
         event.preventDefault();
         fileInput.click();
     });
 
     // Evento de seleção de arquivo
-    fileInput.addEventListener('change', function(event) {
+    fileInput.addEventListener('change', function (event) {
         const file = event.target.files[0];
         if (file) {
             const reader = new FileReader();
-            reader.onload = function(e) {
+            reader.onload = function (e) {
                 const imageData = e.target.result;
                 avatar.src = imageData;
                 saveAvatar(imageData);
@@ -119,43 +113,89 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     // Evento do botão confirmar
-    confirmButton.addEventListener('click', function(event) {
+    confirmButton.addEventListener('click', function (event) {
         event.preventDefault();
-        saveFormData();
-        nomeDisplay.textContent = fields.nome.value;
-        nomeDisplay.classList.remove('hidden');
-        nomeInput.classList.add('hidden');
+        nomeUsuario.innerHTML = fields.campoNome.value;
 
-        // Esconde os campos
-        fields.email.classList.add('hidden');
-        fields.telefone.classList.add('hidden');
-        fields.profissao.classList.add('hidden');
-        fields.localizacao.classList.add('hidden');
-        fields.biografia.classList.add('hidden');
+        if (indexUsuario < 0) {
+            alert('E-mail não encontrado');
+        } else {
+            usuarios[indexUsuario].emailCad = campoEmail.value;
+            usuarios[indexUsuario].cellphoneNumberCad = campoTelefone.value;
+            usuarios[indexUsuario].biografia = campoBiografia.value;
+            usuarios[indexUsuario].cidade = campoCidade.value;
+            usuarios[indexUsuario].profissao = campoProfissao.value;
+            usuarios[indexUsuario].namePersonCad = campoNome.value;
+
+            usuarioLogado = campoEmail.value;
+
+            localStorage.setItem('usuarioLogado', usuarioLogado);
+            localStorage.setItem('listUser', JSON.stringify(usuarios));
+
+            window.location.href = '../index.html';
+        }
     });
 
     // Evento do botão cancelar
-    cancelButton.addEventListener('click', function(event) {
+    cancelButton.addEventListener('click', function (event) {
         event.preventDefault();
         Object.keys(fields).forEach(key => {
             fields[key].value = '';
         });
+
+        form.reset();
+
         avatar.src = '../assets/ilustrações/Avatar.svg';
-        localStorage.clear();
-        nomeDisplay.textContent = 'Fulano de tal da Silva';
-        nomeDisplay.classList.remove('hidden');
-        nomeInput.classList.add('hidden');
+        nomeUsuario.textContent = 'Fulano de tal da Silva';
+        nomeUsuario.classList.remove('hidden');
+        nomeUsuario.classList.add('hidden');
 
         // Mostra os campos novamente
-        fields.email.classList.remove('hidden');
-        fields.telefone.classList.remove('hidden');
-        fields.profissao.classList.remove('hidden');
-        fields.localizacao.classList.remove('hidden');
-        fields.biografia.classList.remove('hidden');
+        fields.campoEmail.classList.remove('hidden');
+        fields.campoTelefone.classList.remove('hidden');
+        fields.campoProfissao.classList.remove('hidden');
+        fields.campoCidade.classList.remove('hidden');
+        fields.campoBiografia.classList.remove('hidden');
     });
 
     // Eventos de entrada para validação dos campos
-    fields.telefone.addEventListener('input', validateTelefone);
-    fields.email.addEventListener('input', validateEmail);
-    fields.biografia.addEventListener('input', validateBiografia);
+    fields.campoTelefone.addEventListener('input', validatecampoTelefone);
+    fields.campoEmail.addEventListener('input', validatecampoEmail);
+    fields.campoBiografia.addEventListener('input', validatecampoBiografia);
+
+
+    // Função para validar o campoTelefone
+    function validatecampoTelefone() {
+        let campoTelefone = fields.campoTelefone.value.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
+        if (campoTelefone.length > 2) {
+            campoTelefone = `(${campoTelefone.substring(0, 2)}) ${campoTelefone.substring(2)}`;
+        }
+        fields.campoTelefone.value = campoTelefone;
+
+        if (campoTelefone.replace(/\D/g, '').length < 11) {
+            fields.campoTelefone.style.color = 'red';
+        } else {
+            fields.campoTelefone.style.color = '';
+        }
+    }
+
+    // Função para validar o campoemail
+    function validatecampoEmail() {
+        const campoEmail = fields.campoEmail.value;
+        if (!campoEmail.includes('@')) {
+            fields.campoEmail.style.color = 'red';
+        } else {
+            fields.campoEmail.style.color = '';
+        }
+    }
+
+    // Função para validar a campoBiografia
+    function validatecampoBiografia() {
+        const campoBiografia = fields.campoBiografia.value;
+        if (campoBiografia.length < 20) {
+            fields.campoBiografia.style.color = 'red';
+        } else {
+            fields.campoBiografia.style.color = '';
+        }
+    }
 });
